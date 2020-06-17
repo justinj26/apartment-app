1358bea64f94e5f8015830e9720199879aaabb3e
justin_james <jjames11799@gmail.com>
2020-06-17 15:48:31 -0400

    Feeling done

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,3 +1,33 @@
 class ApartmentsController < ApplicationController
   # add controller methods
+  
+
+  get '/apartments' do
+    @apartments = Apartment.all 
+    erb :"apartments/index" 
+  end
+
+  get '/apartments/new' do 
+    erb :"apartments/new"
+  end 
+  get '/apartments/:id' do 
+    @apartment = find_apartment 
+    
+    erb :"apartments/show"
+  end
+  
+  post '/apartments' do 
+    apartment = Apartment.create(params)
+    redirect '/apartments'
+  end
+
+  delete '/apartments/:id' do 
+    apartment = find_apartment
+    apartment.destroy 
+    redirect '/apartments'
+  end 
+
+  def find_apartment
+    Apartment.find(params[:id])
+  end
 end
--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -1,3 +1,55 @@
 class TenantsController < ApplicationController
   # add controller methods
+
+  get '/tenants' do 
+    @tenants = Tenant.all
+    erb :"tenants/index"
+  end
+
+  get '/tenants/new' do 
+    @apartments = Apartment.all
+    erb :'tenants/new'
+  end 
+
+  post '/tenants' do 
+    tenant = Tenant.new(params[:tenant])
+
+    unless params[:apartment][:address].empty?
+      tenant.apartment = Apartment.create(address: params[:apartment][:address])
+    end 
+    tenant.save 
+    redirect '/tenants'
+  end 
+
+  delete '/tenants/:id' do 
+    tenant = find_tenant
+    tenant.destroy 
+    redirect '/tenants'
+  end 
+
+  get '/tenants/:id' do
+    @tenant = find_tenant
+    @apartments = Apartment.all 
+    erb :"tenants/edit"
+  end
+
+  patch '/tenants/:id' do
+      
+    @tenant = find_tenant
+    @tenant.update(name: params[:tenant][:name])
+    
+    unless params[:apartment][:address].empty?
+      @apartment = Apartment.create(address: params[:apartment][:address])
+      @tenant.apartment = @apartment
+    else 
+      @tenant.apartment.id = params[:tenant][:apartment_id]
+    end
+    @tenant.save 
+    redirect '/tenants'
+  end
+
+
+  def find_tenant
+    Tenant.find(params[:id])
+  end
 end
